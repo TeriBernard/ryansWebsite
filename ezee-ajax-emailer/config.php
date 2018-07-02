23c3123399947e2a0d36240ec440a9c9d7ec3427
smackjax <smackjax@gmail.com>
2018-07-02 12:32:08 -0400

    Fix ajax mailer to return fatal errors as json

--- ezee-ajax-emailer/config.php
+++ ezee-ajax-emailer/config.php
@@ -6,37 +6,35 @@ global $ezee_email_vals;
 $name = $ezee_email_vals['name'];
 $email = $ezee_email_vals['email'];
 
-// Configuration for where the email will be sent from (required)
+// Where the email will be sent from (required)
 $ezee_email_send_from_config = [
-    'encryption_type' => 'tls', 
+    'encryption_type' => 'tls', // (optional) 'ssl' or 'tls' 
     'port' => 587, 
-    'server' => 'smtp.gmail.com', 
-    'email' => [$my_secret_email, "Ryan's Rim Restoration"], 
-    'password' => $my_secret_pass, 
-
+    'server' => 'smtp.gmail.com', // Can also take secondary server separated by a comma
+    'email' => [$my_secret_email, 'Ryan\s Rim Restoration'],
+    'password' => $my_secret_pass // Password for address server
 ];
 
 // Config for where the email will be sent to (required)
 $ezee_email_send_to_config = [
+    // If you want to send a 'name' when you have only one recipient,
+    // this still needs to be an array of arrays
     'addresses' => [
-        [$my_secret_email, "Ryan's Rim Restoration"]
+        [$my_secret_email, 'Ryan\s Rim Restoration'],
     ],
-    'subject' => "Contact from $name", 
-    'reply_to' => $email
+    'subject' => "Contact from $name",
+    'reply_to' => [$email, $name]
 ];
 
 // (required)
 $ezee_email_value_options = [
-    // Only send in email the values under 'required_values'
-    'limit_to_required' => true,
-    'fail_on_value_overload' => true,
-    // Required posted keys and values
+    // 'limit_to_required' => false, (defaults true)
+    // 'fail_on_value_overload' => false, (defaults true)
+    // Using this without flags or required keys is not as safe
     'required_values' => [
-            "name" => null,
-            "phone" => null,
-            "email"=> null,
-            "message" => null
+        'name' => null,
+        'phone' => null,
+        'email' => null,
+        'message' => null,
     ]
 ];
-
-
--- js/handle-contact-form.js
+++ js/handle-contact-form.js
@@ -24,38 +24,46 @@ function handleContactForm(formVals, formElement) {
     .then(resData=>{
         // Hide spinner
         statusLoading.classList.remove(fadeReveal);
-        if(resData.status === 'success'){
-            var successElem = document.getElementById('contact-status-success');
-            // Show emailed values
-            var nameResult = document.getElementById('sent-name').innerHTML = 
-                resData.data.sanitized.name;
-            var phoneResult = document.getElementById('sent-phone').innerHTML =
-                resData.data.sanitized.phone;
-            var emailResult = document.getElementById('sent-email').innerHTML =
-                resData.data.sanitized.email;
-            successElem.classList.add(fadeReveal);
+        if((resData === null) || (typeof resData !== 'object') || !resData['status']){
+            console.log(resData);
+            throw Error('Got invalid JSend');
         } else {
-            // Get form inputs
-            var nameInput = document.getElementById('contact-name');
-            var phoneInput = document.getElementById('contact-phone');
-            var emailInput = document.getElementById('contact-email');
-            var messageInput = document.getElementById('contact-message');
+            if(resData['status'] === 'success'){
+                var successElem = document.getElementById('contact-status-success');
+                // Show emailed values
+                var nameResult = document.getElementById('sent-name').innerHTML = 
+                    resData.data.sanitized.name;
+                var phoneResult = document.getElementById('sent-phone').innerHTML =
+                    resData.data.sanitized.phone;
+                var emailResult = document.getElementById('sent-email').innerHTML =
+                    resData.data.sanitized.email;
+                successElem.classList.add(fadeReveal);
+            } else if(resData['status'] === 'fail'){
+                if(resData.data.failed){
+                    var nameInput = document.getElementById('contact-name');
+                    var phoneInput = document.getElementById('contact-phone');
+                    var emailInput = document.getElementById('contact-email');
+                    var messageInput = document.getElementById('contact-message');
 
-            if(resData.data.failed){
-                var failed = resData.data.failed;
-                setInvalid(failed['name'], nameInput);
-                setInvalid(failed['phone'], phoneInput);
-                setInvalid(failed['email'], emailInput);
-                setInvalid(failed['message'], messageInput);
+                    var failed = resData.data.failed;
+                    setInvalid(failed['name'], nameInput);
+                    setInvalid(failed['phone'], phoneInput);
+                    setInvalid(failed['email'], emailInput);
+                    setInvalid(failed['message'], messageInput);
+                }
+                submitBtn.disabled = false;
+                formBlock.classList.add(fadeReveal);
+                failElem.classList.add(fadeReveal);
+            } else {
+                if(!resData['message']) throw Error("Status not 'success' or 'fail', but no 'message' in object");
+                throw Error(resData['message']);
             }
-            submitBtn.disabled = false;
-            formBlock.classList.add(fadeReveal);
-            failElem.classList.add(fadeReveal);
         }
     })
     // Network failure  
     .catch(e=>{
         console.log(e.message);
+        statusLoading.classList.remove(fadeReveal);
         var errorElem = document.getElementById("contact-status-error");
         errorElem.classList.add(fadeReveal);
     })
